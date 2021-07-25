82faee21db9ac4046f921684fa1e2a9f2ce20aba
Micah Cantor <micahcantor01@gmail.com>
2021-07-24 20:47:06 -0400

    update canvas

--- src/Canvas.hs
+++ src/Canvas.hs
@@ -1,7 +1,8 @@
 module Canvas where
 
 import Types ( Color(..), Canvas )
-import Color ( cMult, toPPM )
+import Color ( cMult )
+import qualified Color (toPPM)
 import Data.Matrix (Matrix (..), matrix, setElem, toLists, getElem)
 import System.IO (IOMode (WriteMode), hPutStrLn, withFile)
 
@@ -9,7 +10,7 @@ initCanvas :: Int -> Int -> Canvas
 initCanvas width height = matrix height width (\_ -> Color 0 0 0)
 
 setPixel :: Color -> Int -> Int -> Canvas -> Canvas
-setPixel co x y = setElem co (y, x)
+setPixel co x y = setElem co (y + 1, x + 1) -- Data.Matrix is 1-indexed
 
 canvasHeight :: Canvas -> Int
 canvasHeight = nrows
@@ -20,21 +21,18 @@ canvasWidth = ncols
 canvasDimensions :: Canvas -> (Int, Int)
 canvasDimensions c = (canvasWidth c, canvasHeight c)
 
-(!) :: Canvas -> (Int,  Int) -> Color
-canvas ! (x, y) = getElem y x canvas
+(!) :: Canvas -> (Int, Int) -> Color
+canvas ! (x, y) = getElem (y + 1) (x + 1) canvas
+
+toPPM :: Canvas -> String
+toPPM c = header ++ body
+  where
+    maxColor = 255
+    w = show (canvasWidth c)
+    h = show (canvasHeight c)
+    header = unlines ["P3", w ++ " " ++ h, show maxColor]
+    rowToString = unwords . map (Color.toPPM maxColor)
+    body = unlines $ map rowToString (toLists c)
 
 writeCanvas :: FilePath -> Canvas -> IO ()
-writeCanvas fp c = do
-  let maxColor = 255
-      w = show (canvasWidth c)
-      h = show (canvasHeight c)
-      header = unwords ["P3", "\n", w, h, "\n", show maxColor]
-      rowToString = unwords . map (toPPM maxColor . cMult (fromIntegral maxColor))
-      ppmLines = map rowToString (toLists c)
-  withFile
-    fp
-    WriteMode
-    ( \handle -> do
-        hPutStrLn handle header
-        mapM_ (hPutStrLn handle) ppmLines
-    )
+writeCanvas fp = writeFile fp . toPPM
