7f586bc8b9ed585ac31a46eecdf377ae3f913456
Micah Cantor <micahcantor01@gmail.com>
2021-07-24 20:47:55 -0400

    clamp rgb values between 0 and 255

--- src/Color.hs
+++ src/Color.hs
@@ -4,9 +4,12 @@ import Types (Color (..))
 
 toPPM :: Int -> Color -> String
 toPPM maxColor (Color r g b) =
-  unwords $ map (show . roundTo maxColor . round) [r, g, b]
+  unwords $ map (show . clamp maxColor 0 . round . (* fromIntegral maxColor)) [r, g, b]
   where
-    roundTo x n = if n > x then x else n
+    clamp hi lo x
+      | x < lo = lo
+      | x > hi = hi
+      | otherwise = x
 
 cAdd :: Color -> Color -> Color
 cAdd (Color r1 g1 b1) (Color r2 g2 b2) = Color (r1 + r2) (g1 + g2) (b1 + b2)
